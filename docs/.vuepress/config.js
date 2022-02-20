2611198be3209007ba4a50b492d9743c8656df41
Moumirrai <72272440+Moumirrai@users.noreply.github.com>
2022-02-20 12:06:50 +0100

    2. otázka

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,29 @@
 module.exports = {
     // site config
-    lang: 'en-US',
-    title: 'Bootleg!',
+    lang: 'cs',
+    title: 'NESTLING',
     description: 'No, i dont think so',
   
     // theme and its config
     theme: '@vuepress/theme-default', 
     themeConfig: {
-        logo: 'https://vuejs.org/images/logo.png',
+        //logo: 'https://vuejs.org/images/logo.png',
         lastUpdated: false,
         sidebar: [
           ["/", "Home"],
           "/otázky/1/",
-          "/demos/local/",
-          "/demos/remote/",
-          "/demos/remote-async/",
-          "/demos/native/",
-          "/demos/socket-io/",
-          ["/demos/homepage/", "A Homepage Fit for Heroes"],
-          "/demos/netlify/",
-          "/demos/partials/",
-          "/demos/diagrams/",
-          "/demos/tweet/",
-          "/demos/video/",
-          "/demos/extending/",
+          "/otázky/2/",
+          "/otázky/3/",
+          "/otázky/4/",
+          "/otázky/5/",
+          "/otázky/6/",
         ],
         nav: [
           { text: "Home", link: "/" }
         ],
         repo: "colwilson/vuepress-examples",
         repoLabel: "Contribute",
-        displayAllHeaders: true
+        displayAllHeaders: false
       },
       plugins: [
         [
@@ -41,7 +34,10 @@ module.exports = {
                 placeholder: 'Search',
               },
             },
+            hotKeys: ['ctrl', 'k'],
+            maxSuggestions: 10,
           },
         ],
+        ['vuepress-plugin-smooth-scroll'],
       ],
   }
