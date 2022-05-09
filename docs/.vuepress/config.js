f5d58cb9e15a414bf7fb887a4d06ea14616076b2
FrantaJestrabek <101344323+FrantaJestrabek@users.noreply.github.com>
2022-05-09 21:35:33 +0200

    Zobrazení 33

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -77,6 +77,7 @@ module.exports = {
             "/pos/30/",
             "/pos/31/",
             "/pos/32/",
+            "/pos/33/",
             "/pos/34/",
             "/pos/35/",
             "/pos/36/",
